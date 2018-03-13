7edbbc165852c1d4ed7720ab7264497a46d50913
Marc Molina Torres <marcmolina_10@hotmail.com>
2018-03-13 18:02:06 +0100

    Preparado para trabajar en server y client

--- UNO/GameClient/GameClient.cpp
+++ UNO/GameClient/GameClient.cpp
@@ -8,29 +8,22 @@
 #include <mutex>
 
 #define MAX_MESSAGES 30
+#define DEFAULT_PORT 5000
+std::mutex mutex;
 
-void Receive(std::vector<sf::TcpSocket*> socket, std::vector<std::string>* messages, sf::SocketSelector* ss) {
+void Receive(sf::TcpSocket* socket, size_t* received, std::vector<std::string>* messages, sf::RenderWindow* window) {
 
-	std::string msn;
-	size_t recived;
-	char buffer[2000];
+	while (window->isOpen()) {
+		char buffer[2000];
+		sf::Socket::Status status = socket->receive(buffer, sizeof(buffer), *received);
+		std::string str = buffer;
 
-	while (ss->wait()) {
-
-		for (int i = 0; i < socket.size(); i++) {
-
-			if (ss->isReady(*socket[i])) {
-
-				sf::Socket::Status st = socket[i]->receive(buffer, sizeof(buffer), recived);
-
-				if (st == sf::Socket::Status::Done) {
-					msn = buffer;
-					messages->push_back(msn);
-
-					if (messages->size() > 25)
-						messages->erase(messages->begin(), messages->begin() + 1);
-				}
-			}
+		if (status == sf::Socket::Status::Done) {
+			mutex.lock();
+			messages->push_back(str);
+			if (messages->size() > MAX_MESSAGES)
+				messages->erase(messages->begin(), messages->begin() + 1);
+			mutex.unlock();
 		}
 	}
 }
@@ -39,77 +32,16 @@ int main() {
 
 	PlayerInfo playerInfo;
 
-	// Variables
 	sf::TcpSocket socket;
-	sf::TcpListener listener;
-	sf::SocketSelector ss;
-	sf::Socket::Status socketStatus;
-	sf::Packet packet;
-	std::vector<Direction> directionList;
-	std::vector<sf::TcpSocket*> socketList;
+	sf::TcpSocket::Status socketStatus;
+	size_t received;
+	sf::IpAddress ip = sf::IpAddress::getLocalAddress(); // ip local
+	socket.connect(ip, DEFAULT_PORT);
 
-	sf::IpAddress ip = sf::IpAddress::getLocalAddress();
-	socket.connect(ip, 50000);
-
-	socketStatus = socket.receive(packet);
-	if (socketStatus != sf::TcpSocket::Status::Done)
-		std::cout << "Error" << std::endl;
-	else {
-
-		int size;
-		packet >> size;
-
-		if (size != 0) {
-
-			for (int i = 0; i < size; i++) {
-
-				//Receive packets
-				socketStatus = socket.receive(packet);
-
-				if (socketStatus != sf::TcpSocket::Status::Done)
-					std::cout << "Error";
-				else {
-					Direction direction;
-					packet >> direction.ip >> direction.port;
-					directionList.push_back(direction);
-				}
-			}
-
-			// For each direction
-			for (Direction i : directionList) {
-
-				sf::TcpSocket* newSocket = new sf::TcpSocket();
-				newSocket->connect(i.ip, i.port);
-
-				ss.add(*newSocket);
-				socketList.push_back(newSocket);
-			}
-		}
-
-		std::cout << "Se ha conectado el cliente " << size + 1 << "/4" << std::endl;
-	}
-
-	int portLocal = socket.getLocalPort();
-
-	// Free
-	socket.disconnect();
-	listener.listen(portLocal);
-
-	for (int i = socketList.size(); i < 3; i++) {
-		sf::TcpSocket* s = new sf::TcpSocket();
-
-		listener.accept(*s);
-		socketList.push_back(s);
-		ss.add(*s);
-	}
-
-	listener.close();
-	std::vector<std::string> aMensajes;
-
+	std::vector<std::string> messages;
 	sf::Vector2i screenDimensions(800, 600);
-
 	sf::RenderWindow window;
-	window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "Chat");
+	window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "Cliente");
 
 	sf::Font font;
 	if (!font.loadFromFile("courbd.ttf"))
@@ -130,15 +62,18 @@ int main() {
 	separator.setFillColor(sf::Color(200, 200, 200, 255));
 	separator.setPosition(0, 550);
 
-	std::string msn;
+	std::string messageStr;
 
-	// Thread
-	std::thread t(Receive, socketList, &aMensajes, &ss);
+	// Receive
+	std::thread t(Receive, &socket, &received, &messages, &window);
 
-	while (window.isOpen()) {
+	while (window.isOpen())
+	{
 		sf::Event evento;
-		while (window.pollEvent(evento)) {
-			switch (evento.type) {
+		while (window.pollEvent(evento))
+		{
+			switch (evento.type)
+			{
 			case sf::Event::Closed:
 				window.close();
 				break;
@@ -147,17 +82,21 @@ int main() {
 					window.close();
 				else if (evento.key.code == sf::Keyboard::Return)
 				{
-					aMensajes.push_back(mensaje);
-					if (aMensajes.size() > MAX_MESSAGES) {
-						aMensajes.erase(aMensajes.begin(), aMensajes.begin() + 1);
-					}
-					// SEND
-					msn = mensaje;
+					// Send
+					messageStr = "Cliente:"; //+ std::to_string(userNum);
+					messageStr += mensaje;
+					socketStatus = socket.send(messageStr.c_str(), messageStr.size() + 1);
 
-					for (sf::TcpSocket* s : socketList) {
-						socketStatus = s->send(msn.c_str(), msn.size() + 1);
-						if (socketStatus != sf::TcpSocket::Status::Done)
-							std::cout << "Error" << std::endl;
+					if (messageStr == ">exit" || messageStr == " >exit") {
+						socket.disconnect();
+						exit(0);
+					}
+
+					if (socketStatus == sf::TcpSocket::Status::Disconnected) {
+						messageStr = "Servidor desconectado!";
+						messages.push_back(messageStr);
+						if (messages.size() > MAX_MESSAGES)
+							messages.erase(messages.begin(), messages.begin() + 1);
 					}
 
 					mensaje = ">";
@@ -173,8 +112,8 @@ int main() {
 		}
 
 		window.draw(separator);
-		for (size_t i = 0; i < aMensajes.size(); i++) {
-			std::string chatting = aMensajes[i];
+		for (size_t i = 0; i < messages.size(); i++) {
+			std::string chatting = messages[i];
 			chattingText.setPosition(sf::Vector2f(0, 20 * i));
 			chattingText.setString(chatting);
 			window.draw(chattingText);
@@ -188,8 +127,8 @@ int main() {
 	}
 
 	// Cleanup
-	ss.clear();
-	t.join();
 	socket.disconnect();
+	t.join();
+
 	return 0;
 }
--- UNO/GameServer/GameServer.cpp
+++ UNO/GameServer/GameServer.cpp
@@ -7,47 +7,111 @@
 #include <vector>
 #include <thread>
 #include <mutex>
+#include "Deck.h"
 
 #define MAX_CLIENTS 4
+#define MAX_MESSAGES 30
+#define DEFAULT_PORT 5000
+int userNum = 1;
+void Disconnected(std::vector<sf::TcpSocket*>& list, sf::SocketSelector *ss, int userNum);
 
 int main() {
 
 	PlayerInfo playerInfo;
 
-	// Listener that will wait for the 4 clients (corresponding port)
+	sf::SocketSelector sSelector;
+	//Jugadors llista sockets
+	std::vector<sf::TcpSocket*> sList;
+
+	sf::TcpSocket::Status sStatus;
 	sf::TcpListener listener;
-	listener.listen(50000);
-	std::vector<Direction> dList;
+	sf::TcpListener::Status lStatus;
 
-	// Wait for connections
-	for (int i = 0; i < MAX_CLIENTS; i++) {
+	char buffer[2000];
+	std::string message;
+	size_t received;
 
-		sf::TcpSocket socket;
-		sf::Packet packet;
-		listener.accept(socket);
+	lStatus = listener.listen(DEFAULT_PORT);
+	sSelector.add(listener); // pass listener to selector
 
-		// Number of packets it will receive
-		int size = dList.size();
-		std::cout << "Numero total de clientes: " << size + 1 << std::endl;
-		packet << size;
-		socket.send(packet);
+	while (lStatus != sf::TcpListener::Status::Disconnected) {
 
-		if (i != 0) {
-			// Send for each
-			for (int j = 0; j < dList.size(); j++) {
-				packet.clear();
-				packet << dList[j].ip << dList[j].port;
-				socket.send(packet);
+		// while not disconnected wait for a message from socket
+		while (sSelector.wait()) {
+
+			// check if it's listener
+			if (sSelector.isReady(listener)) {
+				if (sList.size < 4) {
+					sf::TcpSocket* socket = new sf::TcpSocket();
+					sf::TcpListener::Status st = listener.accept(*socket);
+					if (st == sf::TcpListener::Status::Done) {
+						sList.push_back(socket);
+					}
+
+
+					sSelector.add(*socket);
+
+					message = "Se ha conectado el cliente " + std::to_string(userNum) + "!";
+					for (sf::TcpSocket* s : sList)
+						if (s != socket)s->send(message.c_str(), message.size() + 1);
+
+					std::cout << "Se ha conectado el cliente " + std::to_string(userNum) + "!" << std::endl;
+					userNum++;
+				}
+				else {
+					std::cout << "Ya hay 4 jugadores connectados, esperate a que termine de jugar alguien " << std::endl;
+				}
+				
+			}
+
+			// if not listener, socket
+			else {
+
+				for (int i = 0; i < sList.size(); i++) {
+
+					if (sSelector.isReady(*sList[i])) {
+
+						// Receive
+						sStatus = sList[i]->receive(buffer, sizeof(buffer), received);
+
+						if (sStatus == sf::TcpSocket::Status::Done) {
+							message = buffer;
+							for (int j = 0; j < sList.size(); j++) {
+								sf::TcpSocket::Status st = sList[j]->send(message.c_str(), message.size() + 1);
+
+								if (st == sf::TcpSocket::Status::Error)
+									std::cout << "Error" << std::endl;
+							}
+						}
+						else if (sStatus == sf::TcpSocket::Status::Disconnected)
+							Disconnected(sList, &sSelector, i);
+						break;
+					}
+				}
 			}
 		}
-
-		Direction direction((socket.getRemoteAddress()).toString(), socket.getRemotePort());
-		dList.push_back(direction); // Add it to our vector
-
-		socket.disconnect();
 	}
 
 	// Cleanup
+	sSelector.clear();
 	listener.close();
+	for (sf::TcpSocket* &socket : sList)
+		socket->disconnect();
+	sList.clear();
+	
 	return 0;
+}
+
+void Disconnected(std::vector<sf::TcpSocket*>& list, sf::SocketSelector *ss, int userNum) {
+	ss->remove(*list[userNum]); // remove from ss, because it has disconnected
+	list[userNum]->disconnect(); // disconnect
+
+	delete(list[userNum]);
+	list.erase(list.begin() + userNum, list.begin() + userNum + 1);
+
+	for (sf::TcpSocket* socket : list) {
+		std::string outMsn = "El cliente " + std::to_string(userNum + 1) + " se ha desconectado!";
+		socket->send(outMsn.c_str(), outMsn.size() + 1);
+	}
+	std::cout << "El cliente " + std::to_string(userNum + 1) + " se ha desconectado!" << std::endl;
 }
